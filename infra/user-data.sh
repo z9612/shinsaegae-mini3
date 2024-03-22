669024421e2c606e37dd51c3b0efa6bcf1214594
giunchoe <z961223@gmail.com>
2024-03-23 02:26:16 +0900

    userdata 수정

--- infra/user-data.sh
+++ infra/user-data.sh
@@ -107,4 +107,5 @@ CWAgentConfig
 
 sudo dnf install collectd -y
 # Start CloudWatch Agent
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+cd /opt/aws/amazon-cloudwatch-agent/bin
+sudo ./amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:./config.json
