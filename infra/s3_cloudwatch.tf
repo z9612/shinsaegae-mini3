9b4b92ca9453b5de94e703a2b8623bae3dd67d0a
giunchoe <z961223@gmail.com>
2024-03-23 17:54:41 +0900

    cloudwatch 1차 검수

--- infra/s3_cloudwatch.tf
+++ infra/s3_cloudwatch.tf
@@ -3,37 +3,10 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = "giun-terraform-log-bucket"
 }
 
-# IAM 정책은 누가 어떤 것을 할 수 있는지를 정의
-# CloudWatch Logs 서비스가 목표 S3 버킷에 객체를 업로드할 수 있는 권한이 포함
-resource "aws_iam_policy" "s3_policy" {
-  name = "allow-cloudwatch-logs-to-s3"
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject"
-      ],
-      "Resource": [
-        "${aws_s3_bucket.s3_bucket.arn}/*"
-      ]
-    }
-  ]
-}
-POLICY
-}
-
-# CloudWatch Log Group
-resource "aws_cloudwatch_log_group" "terraform-lg" {
-  name = "terraform-log-group"
-}
-
-# Firehose IAM Role
+#Firehose 가 가지는 역할
 resource "aws_iam_role" "firehose_role" {
-  name               = "firehose_role"
-  assume_role_policy = <<EOF
+  name               = "FirehosetoS3Role"
+  assume_role_policy = <<-EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -56,32 +29,46 @@ resource "aws_iam_role" "firehose_role" {
 EOF
 }
 
+
 # Firehose가 S3에 쓸 수 있는 정책
 resource "aws_iam_policy" "firehose_s3_policy" {
   name        = "firehose_s3_policy"
   description = "Allows Firehose to write to S3"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-       Action   = "firehose:*",
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-       Action   = "s3:*",
-        Resource = "*"
-      }
-    ]
-  })
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+          "s3:AbortMultipartUpload",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:ListBucketMultipartUploads",
+          "s3:PutObject"
+      ],
+      "Resource": [
+          "${aws_s3_bucket.s3_bucket.arn}",
+          "${aws_s3_bucket.s3_bucket.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
 }
 
-# IAM Policy를Role에 붙이기 
+# Firehose IAM Policy를Role에 붙이기
 resource "aws_iam_role_policy_attachment" "firehose_s3_policy_attachment" {
   policy_arn = aws_iam_policy.firehose_s3_policy.arn
   role       = aws_iam_role.firehose_role.name
+}
+
+
+# CloudWatch Log Group 생성
+resource "aws_cloudwatch_log_group" "terraform-lg" {
+  name = "terraform-log-group"
 }
 
 # Firehose 생성
@@ -99,12 +86,53 @@ resource "aws_kinesis_firehose_delivery_stream" "firehose_stream" {
   depends_on = [aws_cloudwatch_log_group.terraform-lg]
 }
 
-# loudWatch Logs 그룹에서 발생하는 로그 이벤트를 선택하고, 해당 이벤트를 특정 대상으로 전송하는 규칙을 정의
+#CloudWatch 의 역할
+resource "aws_iam_role" "cloudwatchrole" {
+  name               = "CWLtoKinesisFirehoseRole"
+  assume_role_policy = <<EOF
+{
+  "Statement": {
+    "Effect": "Allow",
+    "Principal": { "Service": "logs.amazonaws.com" },
+    "Action": "sts:AssumeRole"
+  }
+}
+EOF
+}
+
+#CloudWatch 가 Firehose에 접근하는 정책
+resource "aws_iam_policy" "cloudwatch_firehose_policy" {
+  name        = "cloudwatch_firehose_policy"
+  description = "Policy for allowing PutRecord action on Firehose"
+
+  policy = <<-EOF
+    {
+      "Version": "2012-10-17",
+      "Statement":[
+        {
+          "Effect":"Allow",
+          "Action":["firehose:PutRecord"],
+          "Resource":[
+            "*"
+          ]
+        }
+      ]
+    }
+  EOF
+}
+
+#CloudWatch IAM Policy를 Role에 붙이기
+resource "aws_iam_role_policy_attachment" "cloudwatch_firehose_policy_attachment" {
+  policy_arn = aws_iam_policy.cloudwatch_firehose_policy.arn
+  role       = aws_iam_role.cloudwatchrole.name
+}
+
+# 구독 필터, cloudWatch Logs 그룹에서 발생하는 로그 이벤트를 선택해서 firehose로 전달
 resource "aws_cloudwatch_log_subscription_filter" "log_subscription_filter" {
   name            = "firehose_subscription_filter"
   log_group_name  = aws_cloudwatch_log_group.terraform-lg.name
   filter_pattern  = ""
   destination_arn = aws_kinesis_firehose_delivery_stream.firehose_stream.arn
-  role_arn        = aws_iam_role.firehose_role.arn
+  role_arn        = aws_iam_role.cloudwatchrole.arn
 }
 
