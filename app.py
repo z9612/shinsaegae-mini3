1cc46d239306aa36f7dc5dc6187c15ec8c302692
jaehyeongg <106142614+jaehyeongg@users.noreply.github.com>
2024-03-24 20:22:44 +0900

    Update app.py
    port 80으로 변경

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def update_event():
         return 'Invalid action type'
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True,host='0.0.0.0',port=80)
