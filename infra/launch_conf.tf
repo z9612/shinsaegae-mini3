2e14fc23d474a74da05e7582154ccab46bb268e1
giunchoe <z961223@gmail.com>
2024-03-24 22:27:02 +0900

    user-data 분리

--- infra/launch_conf.tf
+++ infra/launch_conf.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "web" {
  instance_type = "t3.large"
  key_name = aws_key_pair.project-key.key_name
  security_groups = [aws_security_group.terrarform-asg-sg.id]
- user_data = file("user-data.sh")
+ user_data = file("user-data-web.sh")
  iam_instance_profile = aws_iam_instance_profile.cwagent_profile.name #추가
 
  lifecycle {
@@ -18,7 +18,7 @@ resource "aws_launch_configuration" "was" {
  instance_type = "t3.large"
  key_name = aws_key_pair.project-key.key_name
  security_groups = [aws_security_group.terrarform-internal-asg-sg.id]
- user_data = file("user-data.sh")
+ user_data = file("user-data-was.sh")
  lifecycle {
  create_before_destroy = true
  }
