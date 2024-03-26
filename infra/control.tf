b07a628efbb89885814cf4e6f7b184e1ac5502ed
giunchoe <z961223@gmail.com>
2024-03-26 10:20:24 +0900

    control-node ami 수정

--- infra/control.tf
+++ infra/control.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "control" {
-  ami                         = "ami-02c956980e9e063e5"
+  ami                         = "ami-00b23009cd61da5c9"
   instance_type               = "t3.large"
   vpc_security_group_ids      = [aws_security_group.control-sg.id]
   key_name                    = aws_key_pair.project-key.key_name
