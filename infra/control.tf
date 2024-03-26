603214866a591b94d72b1dd04212194aa86ebaba
giunchoe <z961223@gmail.com>
2024-03-26 10:32:59 +0900

    control node userdata 추가

--- infra/control.tf
+++ infra/control.tf
@@ -5,9 +5,15 @@ resource "aws_instance" "control" {
   key_name                    = aws_key_pair.project-key.key_name
   subnet_id                   = aws_subnet.public_subnets[1].id
   associate_public_ip_address = true
- 
-  #contorl node 에는 Admin Access 적용
-  iam_instance_profile = "adminAccess"
+  iam_instance_profile        = "adminAccess"  # contorl node 에는 Admin Access 적용
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum install -y mysql awscli
+                export rds_dns=$(aws rds describe-db-instances --db-instance-identifier terraform-mysql --query 'DBInstances[0].Endpoint.Address' --output text)
+                git clone https://github.com/z9612/shinsaegae-mini3.git
+                sudo mysql -u nana -h $rds_dns terraformdb < /home/ec2-user/shinsaegae-mini3/Dump20240118/project_user.sql -p nana1234
+              EOF
 
   tags = {
     Name = "control-node"
